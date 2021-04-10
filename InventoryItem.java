6a4b2c7d2683c91218f4d72372ec416df31c6b26
Jayantnis <65399679+Jayantnis@users.noreply.github.com>
2021-04-10 22:47:59 +0530

    Add files via upload
        /**         +-:::::-+ Jayant Nishad Internship_in_suvenconsultants-by-Java-Project- { 2 } +-::::::-+      **/
    /*** $~+_**::**_+$ Internship Project Second 2nd Home Inventory Manager Project  $~+_**::**_+$ ***/
    $ Home Inventory Project complete
    with show on output

---
+++ InventoryItem.java
@@ -0,0 +1,16 @@
+package HomeInventory;
+
+public class InventoryItem { ///Inventory item .java
+
+    public String description;
+    public String location;
+    public boolean marked;
+    public  String serialNumber;
+    public  String purchasePrice;
+    public String purchaseDate;
+    public  String purchaseLocation;
+    public String note;
+    public String photoFile;
+
+
+}
